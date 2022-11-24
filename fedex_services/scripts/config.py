0f58d4b307a1c323576f8828c98d3b14575bd426
Solveig Hendryx <114836976+sghendryx@users.noreply.github.com>
2022-11-24 09:32:40 -0700

    Upload Project Base
    Took the wrapper from the FedEx developers and their example code and cut it down to what I needed to get started on configuring it for RC.

---
+++ fedex_services/scripts/config.py
@@ -0,0 +1,20 @@
+"""
+This file holds various configuration options used for all of the scripts.
+
+You will need to change the values below to match your test account.
+"""
+import os
+import sys
+
+# Use the fedex_wrapper directory included in the downloaded package instead of
+# any globally installed versions.
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from fedex_wrapper.config import FedexConfig
+
+# Change these values to match your testing account/meter number.
+CONFIG_OBJ = FedexConfig(key='xxxxxxx',
+                         password='xxxxxxx',
+                         account_number='xxxxxxx',
+                         meter_number='xxxxxxx',
+                         freight_account_number='xxxxxxx',
+                         use_test_server=True)
