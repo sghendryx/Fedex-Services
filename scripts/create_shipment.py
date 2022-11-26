6c44211535eb004d1f09dd37797ce4c05e6fba45
Solveig Hendryx <114836976+sghendryx@users.noreply.github.com>
2022-11-25 23:36:20 -0700

    finished configuring
    finishing up the final command specifications

--- scripts/create_shipment.py
+++ scripts/create_shipment.py
@@ -3,13 +3,122 @@ import logging
 import binascii
 import datetime
 import sys
-import os
+import shutil
 
 from config import CONFIG_OBJ
 from fedex_wrapper.services.ship_service import FedexProcessShipmentRequest
 
+def monitor_shipping_label():
+
+    global recipient_name
+
+    GENERATE_IMAGE_TYPE = 'PDF'
+
+    logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+    customer_transaction_id = "*** ShipService Request v17 using Python ***"  # Optional transaction_id
+    shipment = FedexProcessShipmentRequest(CONFIG_OBJ, customer_transaction_id=customer_transaction_id)
+
+    shipment.RequestedShipment.DropoffType = 'REGULAR_PICKUP'
+    shipment.RequestedShipment.ServiceType = 'FEDEX_2_DAY'
+    shipment.RequestedShipment.PackagingType = 'YOUR_PACKAGING'
+
+    shipment.RequestedShipment.Shipper.Contact.PersonName = 'IT Department'
+    shipment.RequestedShipment.Shipper.Contact.CompanyName = 'Red Canary'
+    shipment.RequestedShipment.Shipper.Contact.PhoneNumber = '8559770686'
+    shipment.RequestedShipment.Shipper.Address.StreetLines = ['1601 19th St Suite 900']
+    shipment.RequestedShipment.Shipper.Address.City = 'Denver'
+    shipment.RequestedShipment.Shipper.Address.StateOrProvinceCode = 'CO'
+    shipment.RequestedShipment.Shipper.Address.PostalCode = '80202'
+    shipment.RequestedShipment.Shipper.Address.CountryCode = 'US'
+    shipment.RequestedShipment.Shipper.Address.Residential = True
+
+    shipment.RequestedShipment.Recipient.Contact.PersonName = recipient_name
+    shipment.RequestedShipment.Recipient.Contact.PhoneNumber = recipient_phone_number
+    shipment.RequestedShipment.Recipient.Address.StreetLines = recipient_address
+    shipment.RequestedShipment.Recipient.Address.City = recipient_city
+    shipment.RequestedShipment.Recipient.Address.StateOrProvinceCode = recipient_state
+    shipment.RequestedShipment.Recipient.Address.PostalCode = recipient_zipcode
+    shipment.RequestedShipment.Recipient.Address.CountryCode = 'US'
+
+    shipment.RequestedShipment.Recipient.Address.Residential = True
+    shipment.RequestedShipment.EdtRequestType = 'NONE'
+    shipment.RequestedShipment.ShippingChargesPayment.Payor.ResponsibleParty.AccountNumber = CONFIG_OBJ.account_number
+    shipment.RequestedShipment.ShippingChargesPayment.PaymentType = 'SENDER'
+
+    shipment.RequestedShipment.LabelSpecification.LabelFormatType = 'COMMON2D'
+    shipment.RequestedShipment.LabelSpecification.ImageType = GENERATE_IMAGE_TYPE
+    shipment.RequestedShipment.LabelSpecification.LabelStockType = 'PAPER_7X4.75'
+
+
+    shipment.RequestedShipment.ShipTimestamp = datetime.datetime.now().replace(microsecond=0).isoformat()
+    shipment.RequestedShipment.LabelSpecification.LabelPrintingOrientation = 'TOP_EDGE_OF_TEXT_FIRST'
+
+    if hasattr(shipment.RequestedShipment.LabelSpecification, 'LabelOrder'):
+        del shipment.RequestedShipment.LabelSpecification.LabelOrder  # Delete, not using.
+
+    package1_weight = shipment.create_wsdl_object_of_type('Weight')
+    package1_insure = shipment.create_wsdl_object_of_type('Money')
+    package1_weight.Value = 15.0
+    package1_weight.Units = "LB"
+
+    # Insured Value of Monitor
+    package1_insure.Currency = 'USD'
+    package1_insure.Amount = 500.0
+
+    # Create PackageLineItem
+    package1 = shipment.create_wsdl_object_of_type('RequestedPackageLineItem')
+    # BAG, BARREL, BASKET, BOX, BUCKET, BUNDLE, CARTON, CASE, CONTAINER, ENVELOPE etc..
+    package1.PhysicalPackaging = 'BOX'
+    package1.Weight = package1_weight
+
+    # Add Insured and Total Insured values.
+    package1.InsuredValue = package1_insure
+    shipment.RequestedShipment.TotalInsuredValue = package1_insure
+
+
+    shipment.add_package(package1)
+    shipment.send_request()
+    print("HighestSeverity: {}".format(shipment.response.HighestSeverity))
+    print("Tracking #: {}"
+        "".format(shipment.response.CompletedShipmentDetail.CompletedPackageDetails[0].TrackingIds[0].TrackingNumber))
+
+
+    CompletedPackageDetails = shipment.response.CompletedShipmentDetail.CompletedPackageDetails[0]
+    if hasattr(CompletedPackageDetails, 'PackageRating'):
+        print("Net Shipping Cost (US$): {}"
+            "".format(CompletedPackageDetails.PackageRating.PackageRateDetails[0].NetCharge.Amount))
+    else:
+        print('WARNING: Unable to get shipping rate.')
+
+    ascii_label_data = shipment.response.CompletedShipmentDetail.CompletedPackageDetails[0].Label.Parts[0].Image
+
+    label_binary_data = binascii.a2b_base64(ascii_label_data)
+
+    recipient_name = recipient_name.replace(' ', '_')
+    recipient_name = recipient_name.replace("'", "")
+    out_path = (f'{recipient_name}_monitor.%s') % GENERATE_IMAGE_TYPE.lower()
+    print("Writing to file {}".format(out_path))
+    out_file = open(out_path, 'wb')
+    out_file.write(label_binary_data)
+    out_file.close()
+
+    dst_path = r"/Users/solveighendryx/downloads"
+    shutil.move(out_path, dst_path)
+
+# Generates the shipping label as a PDF.
+GENERATE_IMAGE_TYPE = 'PDF'
+
+# Un-comment to see the response from Fedex printed in stdout.
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+
+# This is the object that will be handling our shipment request.
+# We're using the FedexConfig object from config.py in this dir.
+customer_transaction_id = "*** ShipService Request v17 using Python ***"  # Optional transaction_id
+shipment = FedexProcessShipmentRequest(CONFIG_OBJ, customer_transaction_id=customer_transaction_id)
+
 print("")
-print("Hello! Please enter the first and last name of the person you are shipping to.")
+print("Enter the first and last name of the person you are shipping to.")
+print("")
 print("Type your answer and hit 'Enter'.")
 recipient_name = input()
 
@@ -32,17 +141,6 @@ recipient_state = input()
 print("")
 print("Zip Code:")
 recipient_zipcode = input()
-
-# Generates the shipping label as a PDF.
-GENERATE_IMAGE_TYPE = 'PDF'
-
-# Un-comment to see the response from Fedex printed in stdout.
-logging.basicConfig(stream=sys.stdout, level=logging.INFO)
-
-# This is the object that will be handling our shipment request.
-# We're using the FedexConfig object from config.py in this dir.
-customer_transaction_id = "*** ShipService Request v17 using Python ***"  # Optional transaction_id
-shipment = FedexProcessShipmentRequest(CONFIG_OBJ, customer_transaction_id=customer_transaction_id)
 
 # This is very generalized, top-level information.
 # REGULAR_PICKUP, REQUEST_COURIER, DROP_BOX, BUSINESS_SERVICE_CENTER or STATION
@@ -134,7 +232,7 @@ if answer == "Laptop" or answer == "laptop":
     print("Are you shipping a monitor with this as well? Y/N")
     answer = input()
     if answer == "Y" or answer == "y":
-        print("generate a monitor shipping label as well")
+        monitor_shipping_label()
     package1_weight.Value = 10.0
     package1_weight.Units = "LB"
 
@@ -217,4 +315,10 @@ print("Writing to file {}".format(out_path))
 out_file = open(out_path, 'wb')
 out_file.write(label_binary_data)
 out_file.close()
-os.system(f"open {out_path}")
+
+#Opens generated PDF
+# os.system(f"open {out_path}")
+
+#Moves generated PDF to downloads.
+dst_path = r"/Users/solveighendryx/downloads"
+shutil.move(out_path, dst_path)
