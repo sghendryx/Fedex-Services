9e5ac0973c7a72c0df4d23f8d00c027f3dfc985d
Solveig Hendryx <114836976+sghendryx@users.noreply.github.com>
2022-11-28 21:49:22 -0700

    beginning configuration
    beginning to configure for our team

--- scripts/create_pickup.py
+++ scripts/create_pickup.py
@@ -12,15 +12,15 @@ pickup_date = datetime.datetime.now().replace(microsecond=0)
 customer_transaction_id = "*** PickupService Request v11 using Python ***"  # Optional transaction_id
 pickup_service = FedexCreatePickupRequest(CONFIG_OBJ, customer_transaction_id)
 
-pickup_service.OriginDetail.PickupLocation.Contact.PersonName = 'Sender Name'
-pickup_service.OriginDetail.PickupLocation.Contact.EMailAddress = 'test@user.com'
-pickup_service.OriginDetail.PickupLocation.Contact.CompanyName = 'Acme Inc.'
-pickup_service.OriginDetail.PickupLocation.Contact.PhoneNumber = '9012638716'
-pickup_service.OriginDetail.PickupLocation.Address.StateOrProvinceCode = 'SC'
-pickup_service.OriginDetail.PickupLocation.Address.PostalCode = '29631'
+pickup_service.OriginDetail.PickupLocation.Contact.PersonName = 'IT Department'
+pickup_service.OriginDetail.PickupLocation.Contact.EMailAddress = 'IT@Redcanary.com'
+pickup_service.OriginDetail.PickupLocation.Contact.CompanyName = 'Red Canary'
+pickup_service.OriginDetail.PickupLocation.Contact.PhoneNumber = '8559770686'
+pickup_service.OriginDetail.PickupLocation.Address.StateOrProvinceCode = 'CO'
+pickup_service.OriginDetail.PickupLocation.Address.PostalCode = '80202'
 pickup_service.OriginDetail.PickupLocation.Address.CountryCode = 'US'
-pickup_service.OriginDetail.PickupLocation.Address.StreetLines = ['155 Old Greenville Hwy', 'Suite 103']
-pickup_service.OriginDetail.PickupLocation.Address.City = 'Clemson'
+pickup_service.OriginDetail.PickupLocation.Address.StreetLines = ['1601 19th St', 'Suite 900']
+pickup_service.OriginDetail.PickupLocation.Address.City = 'Denver'
 # pickup_service.OriginDetail.PickupLocation.Address.UrbanizationCode = ''  # For Puerto Rico only
 pickup_service.OriginDetail.PickupLocation.Address.Residential = False
 
@@ -28,7 +28,7 @@ pickup_service.OriginDetail.PickupLocation.Address.Residential = False
 # pickup_service.OriginDetail.PackageLocation = 'NONE'
 
 # APARTMENT, BUILDING, DEPARTMENT, FLOOR, ROOM, SUITE
-# pickup_service.OriginDetail.BuildingPart = 'SUITE'
+pickup_service.OriginDetail.BuildingPart = 'SUITE'
 
 # Identifies the date and time the package will be ready for pickup by FedEx.
 pickup_service.OriginDetail.ReadyTimestamp = pickup_date.isoformat()
@@ -39,8 +39,14 @@ pickup_service.OriginDetail.CompanyCloseTime = '23:00:00'
 pickup_service.CarrierCode = 'FDXE'
 
 pickup_service.TotalWeight.Units = 'LB'
-pickup_service.TotalWeight.Value = '1'
-pickup_service.PackageCount = '1'
+
+print("")
+print("Welcome to Create Pickup! Please enter the total estimated weight (LBs) of your pikcup and press 'Enter'.")
+print("")
+pickup_service.TotalWeight.Value = input() #If these inputs don't work later both of these data types need to be strings
+
+print("What is the total number of packages you're sending today?")
+pickup_service.PackageCount = input()
 # pickup_service.OversizePackageCount = '1'
 
 # pickup_service.CommodityDescription = ''
@@ -57,25 +63,25 @@ print('Pickup request sent:')
 print(pickup_service.response.HighestSeverity == 'SUCCESS')
 print(pickup_service.response.Notifications[0].Message)
 
-# Cancel the pickup request that we just got confirmation for
-cancel_pickup = FedexCancelPickupRequest(CONFIG_OBJ)
+# # Cancel the pickup request that we just got confirmation for
+# cancel_pickup = FedexCancelPickupRequest(CONFIG_OBJ)
 
-cancel_pickup.PickupConfirmationNumber = pickup_service.response.PickupConfirmationNumber
-# the date for the pickup (eg. '2016-09-26')
-cancel_pickup.ScheduledDate = pickup_date.strftime('%Y-%m-%d')
-cancel_pickup.EndDate = pickup_date.strftime('%Y-%m-%d')
-cancel_pickup.Location = pickup_service.response.Location
-cancel_pickup.Remarks = None
-cancel_pickup.ShippingChargesPayment =  None
-cancel_pickup.Reason = ''
-cancel_pickup.ContactName = 'Sender Name'
-cancel_pickup.PhoneNumber = '9012638716'
-cancel_pickup.PhoneExtension = ''
+# cancel_pickup.PickupConfirmationNumber = pickup_service.response.PickupConfirmationNumber
+# # the date for the pickup (eg. '2016-09-26')
+# cancel_pickup.ScheduledDate = pickup_date.strftime('%Y-%m-%d')
+# cancel_pickup.EndDate = pickup_date.strftime('%Y-%m-%d')
+# cancel_pickup.Location = pickup_service.response.Location
+# cancel_pickup.Remarks = None
+# cancel_pickup.ShippingChargesPayment =  None
+# cancel_pickup.Reason = ''
+# cancel_pickup.ContactName = 'Sender Name'
+# cancel_pickup.PhoneNumber = '9012638716'
+# cancel_pickup.PhoneExtension = ''
 
-cancel_pickup.CarrierCode = 'FDXE'
+# cancel_pickup.CarrierCode = 'FDXE'
 
-cancel_pickup.send_request()
+# cancel_pickup.send_request()
 
-print('Cancel pickup request sent:')
-print('Highest severity:',cancel_pickup.response.HighestSeverity)
-print('Message:',cancel_pickup.response.Notifications[0].Message)
+# print('Cancel pickup request sent:')
+# print('Highest severity:',cancel_pickup.response.HighestSeverity)
+# print('Message:',cancel_pickup.response.Notifications[0].Message)
